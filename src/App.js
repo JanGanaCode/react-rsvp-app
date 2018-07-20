c693fe7d01437a250b6bbdf5e2c6f911dcfe723f
Jan Gana <jan.gana@hiring-hub.com>
2018-07-20 08:12:20 +0100

    Finished Project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import GuestList from './GuestList';
+import Counter from './Counter';
 
 class App extends Component {
   state = {
@@ -70,11 +71,15 @@ class App extends Component {
 
   getTotalInvited = () => this.state.guests.length;
 
-  // getAttendingGuests = () =>
-
-  // getUnconfirmeduests = () => 
+  getAttendingGuests = () => 
+    this.state.guests.reduce(
+      (total, guest) => guest.isConfirmed ? total + 1 : total, 
+      0);
 
   render() {
+    const totalInvited = this.getTotalInvited();
+    const numberAttending = this.getAttendingGuests();
+    const numberUnconfirmed = totalInvited - numberAttending;
     return (
       <div className="App">
         <header>
@@ -104,22 +109,11 @@ class App extends Component {
               /> Hide those who haven't responded
             </label>
           </div>
-          <table className="counter">
-            <tbody>
-              <tr>
-                <td>Attending:</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>Unconfirmed:</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <td>Total:</td>
-                <td>3</td>
-              </tr>
-            </tbody>
-          </table>
+          <Counter 
+            totalInvited={totalInvited}
+            numberAttending={numberAttending}
+            numberUnconfirmed={numberUnconfirmed}
+          />
           <GuestList 
             guests={this.state.guests} 
             toggleConfirmationAt={this.toggleConfirmationAt}
@@ -127,6 +121,7 @@ class App extends Component {
             setNameAt={this.setNameAt}
             isFiltered={this.state.isFiltered}
             removeGuestAt={this.removeGuestAt}
+            pendingGuest={this.state.pendingGuest}
           />
         </div>
       </div>
